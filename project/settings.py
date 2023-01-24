13d47e8c08258991658cec7a0f3e9d0d03164f98
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-01-24 19:04:27 +0100

    download django exstintions

--- project/settings.py
+++ project/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+     'django_extensions',
     "taggit",
     "product",
+    
 ]
 
 MIDDLEWARE = [
@@ -125,6 +127,19 @@ STATICFILES_DIRS = [
 
 MEDIA_URL='media/'
 MEDIA_ROOT=BASE_DIR/'media'
+
+
+
+
+GRAPH_MODELS = {
+  'all_applications': True,
+  'group_models': True,
+}
+ 
+
+GRAPH_MODELS = {
+  'app_labels': ["myapp1", "myapp2", "auth"],
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
