db538839f26adfad9261bdd9877b18a169bcc6ac
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-01-24 16:51:56 +0100

    add new file in models

--- product/models.py
+++ product/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from taggit.managers import TaggableManager
 from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 
@@ -22,20 +24,33 @@ class product(models.Model):
     tags=TaggableManager()
     subtitle=models.TextField(_('subtitle'),max_length=500)
     description=models.TextField(_('description'), max_length=1000)
+    slug=models.SlugField(null=True,blank=True)
     
     def __str__(self):
         return self.name
 class product_image(models.Model):
-    pass
-    '''
-    product:foreginkey
-    image
-    '''
+    product=models.ForeignKey(product,verbose_name=_('product'), related_name='product_image',on_delete=models.CASCADE)
+    image=models.ImageField( _('image'), upload_to='product_images/')
+    def __str__(self):
+        return str(self.product)
+
+
 
 
 class Brand(models.Model):
-    pass
+    name=models.CharField(_('brand'), max_length=100)
+    image=models.ImageField( _('image'), upload_to='brand/')
+
+    def __str__(self):
+        return self.name
 
 
 class reviews(models.Model):
-    pass
+    user=models.ForeignKey(User,verbose_name=_('reviews'), related_name='review_author',on_delete=models.SET_NULL,null=True,blank=True)
+    product=models.ForeignKey(product,verbose_name=_('product'), related_name='product_review',on_delete=models.CASCADE)
+    comment=models.CharField(_('comment'), max_length=300)
+    rate=models.IntegerField(_('rate'))
+    createt_at=models.DateTimeField(_('createt_at'), default=timezone.now)
+
+    def __str__(self):
+        return str(self.product)
