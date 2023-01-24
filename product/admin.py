f5635605da4e8c4964d00c1296adddee5eff8d7e
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-01-24 18:18:56 +0100

    add new branch

--- product/admin.py
+++ product/admin.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import product,product_image,Brand,reviews
 
+
+
+
 admin.site.register(product)
 admin.site.register(product_image)
 admin.site.register(Brand)
