7d95f34273866089dc3977ccd72744bc3b7f08a1
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-01-24 20:14:23 +0100

    add new file to admin site

--- product/admin.py
+++ product/admin.py
@@ -5,8 +5,16 @@ from .models import product,product_image,Brand,reviews
 
 
 
+class productImagesAdmin(admin.TabularInline):
+    model = product_image
 
-admin.site.register(product)
+
+class productAdmin(admin.ModelAdmin):
+    list_display=['name','brand','price']
+    list_filter=['brand','price']
+    inlines=[productImagesAdmin]
+
+admin.site.register(product,productAdmin)
 admin.site.register(product_image)
 admin.site.register(Brand)
 admin.site.register(reviews)
