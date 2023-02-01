068f7b39b25dbca1903f9b081469a72c68d6676f
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-02-01 15:06:32 +0100

    add new files in admin

--- orders/admin.py
+++ orders/admin.py
@@ -4,8 +4,18 @@ from django.contrib import admin
 from .models import order,order_detail,Cart, cart_detail
 
 
-admin.site.register(order)
-admin.site.register(order_detail)
+class orderAdmin(admin.ModelAdmin):
+    list_display=['odrder_code','delivery_date']
+    list_filter=['order_status','order_date']
+
+class order_detailAdmin(admin.ModelAdmin):
+    list_display=['order','product','price','quantity','total']
+    list_filter=['price','quantity']
+
+admin.site.register(order,orderAdmin)
+admin.site.register(order_detail,order_detailAdmin)
+
+
 
 
 
