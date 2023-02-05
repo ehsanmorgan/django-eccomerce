a6450f7ec61217d4844ee132a161bc29b97d5c97
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-02-05 03:36:04 +0100

    add order_list.html fronend

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+
+from .models import order
+
+
+class order_List(ListView):
+    model=order
--- project/urls.py
+++ project/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/',include('product.urls',namespace='products')),
+    path('orders/',include('orders.urls',namespace='orders')),
 ]
 urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
