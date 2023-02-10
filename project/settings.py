dbf137efc9507262a53d6110fcdd064bbc2c4d55
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-02-10 23:22:32 +0100

    add settings app

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
      'django_extensions',
+     "settings",
      "orders",
     "taggit",
     "product",
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'project.urls'
@@ -67,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                 'settings.company_context.get_data',
             ],
         },
     },
