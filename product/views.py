69bf839dd36dfd8afa79e091068a33cfaacd1096
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-02-11 01:36:48 +0100

    add pageination to brand_detail

--- product/views.py
+++ product/views.py
@@ -10,7 +10,7 @@ from .models import Brand
 
 class productList(ListView):
     model=product
-    paginate_by = 30
+    paginate_by = 50
 
 
 class productDetail(DetailView):
@@ -19,16 +19,27 @@ class productDetail(DetailView):
 
 class brand_list(ListView):
     model=Brand
-    paginate_by = 30
+    paginate_by = 50
     queryset=Brand.objects.all().annotate(product_count=Count('product_name'))
    
 
 
-class brand_detail(DetailView):
-    model=Brand
-    
+class brand_detail(ListView):
+    model=product
+    paginate_by = 50
+    template_name='product/brand_detail.html'
+
 
     def get_queryset(self):
-        queryset=Brand.objects.filter(slug=self.kwargs['slug']).annotate(product_count=Count('product_name'))
-
+        brand=Brand.objects.get(slug=self.kwargs['slug'])
+        queryset=product.objects.filter(brand=brand)
         return queryset
+
+
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        data=Brand.objects.filter(slug=self.kwargs['slug']).annotate(product_count=Count('product_name'))[0]
+        print(f'Brand:{data.name}')
+        print(f'Brand:{data.image}')
+        context['brand']=data
+        return context
