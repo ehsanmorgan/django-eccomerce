5ea9d6ad1151a62f96b9d4e58fd850e375c08b33
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-02-21 07:26:29 +0100

    add review at home page

--- settings/views.py
+++ settings/views.py
@@ -9,10 +9,12 @@ def home(request):
     item_sale=product.objects.filter(flag='Sale')[:10]
     item_feature=product.objects.filter(flag='Feature')[:6]
     item_new=product.objects.filter(flag='New')[:12]
+    reviews_2_=reviews.objects.all()
     brands=Brand.objects.all().annotate(product_count=Count('product_name'))
     return render(request,'settings/home.html',{
         'brands':brands,
         'item_sale':item_sale,
         'item_feature':item_feature,
-        'item_new':item_new
+        'item_new':item_new,
+        'reviews_2_':reviews_2_
         })
