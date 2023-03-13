d01214d01c3f0959af59778b5ce49a361972b391
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-03-13 23:44:01 +0100

    add api in order lst

--- orders/api.py
+++ orders/api.py
@@ -11,6 +11,9 @@ class orderListAPI(generics.ListAPIView):
     
     
     
+    
+    
+    
 class cartdetailAPI(generics.RetrieveDestroyAPIView):
     def get(self,request,*args,**kwargs):
         user_name=self.kwargs['username']
