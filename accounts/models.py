385b082dd58754ea900240fc5630217f27f2fc62
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-04-09 18:52:09 +0200

    add class contact number

--- accounts/models.py
+++ accounts/models.py
@@ -22,6 +22,7 @@ class ContactNumber(models.Model):
     typy=models.CharField(max_length=20,choices=number_choices)
     number=models.CharField(max_length=20)
 
+
 adresse_choices=(
     ('Home','Home'),
     ('Office','Office'),
