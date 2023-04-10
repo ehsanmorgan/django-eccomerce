9068b0b0bc8260462c7c1166270af44f87865f42
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-04-10 17:31:41 +0200

    add models  to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Adresse,Profile,ContactNumber
 
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(ContactNumber)
+admin.site.register(Adresse)
+
