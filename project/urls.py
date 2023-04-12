76ca7158e02f23ec4c252a6529684150557e2b40
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-04-12 17:32:51 +0200

    add url accounts

--- project/urls.py
+++ project/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path("accounts/", include("django.contrib.auth.urls")),
     path('admin/', admin.site.urls),
     path('products/',include('product.urls',namespace='products')),
     path('orders/',include('orders.urls',namespace='orders')),
