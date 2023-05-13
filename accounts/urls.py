2468c35ed24f5bb38392087ea4470129239cbba8
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-05-13 19:33:25 +0200

    add sing up in view

---
+++ accounts/urls.py
@@ -0,0 +1,2 @@
+from django.urls import path
+from .views import sing_up,profile
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def sing_up(request):
+   pass
+
+def profile(request):
+    return render( 'accounts/profile.html' ,{})
+
