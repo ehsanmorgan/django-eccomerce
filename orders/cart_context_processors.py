542f9fa59370354476c07166d2858658ce4c4d01
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-05-17 00:30:24 +0200

    add def add to cart

--- orders/cart_context_processors.py
+++ orders/cart_context_processors.py
@@ -2,10 +2,12 @@ from .models import Cart,cart_detail
 
 def get_or_create(request):
     if request.user.is_authenticated:
-        cart,created=Cart.objects.get_or_create(user=request.user,cart_status='Inprogress , Completed')
+        cart,created=Cart.objects.get_or_create(user=request.user,cart_status='Inprogress')
         if not created:
             cart_detail1=cart_detail.objects.filter(cart=cart)
             return{'cart':cart,'cart_detail1':cart_detail1}
         return{'cart':cart}
     else:
         return{}
+    
+    
