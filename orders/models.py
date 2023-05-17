82019662ff61fdc677f2fb14e16f9adcde181d74
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-05-17 18:56:17 +0200

    add to cart

--- orders/models.py
+++ orders/models.py
@@ -45,10 +45,10 @@ class cart_detail(models.Model):
         return str(self.product)
 
 
-    def save(self, *args, **kwargs):
-        self.total= self.quantity * self.price
+    #def save(self, *args, **kwargs):
+        #self.total= self.quantity * self.price
        
-        super(cart_detail, self).save(*args, **kwargs) 
+        #super(cart_detail, self).save(*args, **kwargs) 
 
 
 
--- orders/views.py
+++ orders/views.py
@@ -16,8 +16,15 @@ class order_List(ListView):
     
     
 def add_to_cart(request):
-    pass
-  
+    quantity=request.POST['quantity']
+    product=Product.objects.get(id=request.POST['product_id'])
+    cart=Cart.objects.get(user=request.user , cart_status='Inprogress')
+    cart_details,created=cart_detail.objects.get_or_create(cart=cart , product=product)
+    cart_details.quantity=quantity
+    cart_details.price=product.price
+    cart_details.total = int(quantity) * product.price
+    cart_details.save()
+    return redirect(f'/products/{product.slug}')
 
 
 
