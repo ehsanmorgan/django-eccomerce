507efb1654392a2698340d3b0274dc1e44a809f1
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-05-22 21:39:22 +0200

    add redis with localhost

--- project/settings.py
+++ project/settings.py
@@ -202,5 +202,5 @@ AUTHENTICATION_BACKENDS=['accounts.backends.EmailBackend']
 
 
 
-BROKER_URL = 'redis://redis:6379/0'
+BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0 ' 
