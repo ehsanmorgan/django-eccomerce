a1a24b0fdc0f95b046f0b0e5c236a518d02a8cc1
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-05-29 03:44:54 +0200

    add docker

--- project/settings.py
+++ project/settings.py
@@ -203,10 +203,15 @@ AUTHENTICATION_BACKENDS=['accounts.backends.EmailBackend']
 
 
 
+BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0' 
 
 
-BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0 ' 
+#BROKER_URL = 'redis://localhost:6379/0'
+#CELERY_RESULT_BACKEND = 'redis://localhost:6379/0 ' 
+
+
+
 
 
 
